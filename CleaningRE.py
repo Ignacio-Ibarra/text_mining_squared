778ca757201b7cbbd16c36e16f34ef1e08b0ac1b
Ignacio Ibarra <ignacioibarra.data.science@gmail.com>
2021-09-07 18:01:36 -0300

    changes

--- CleaningRE.py
+++ CleaningRE.py
@@ -4,6 +4,8 @@ Created on Mon Sep  6 13:24:49 2021
 
 @author: Asus
 """
+import pandas as pd
+import numpy as np
 import string
 import unicodedata
 import re
@@ -14,11 +16,36 @@ def strip_accents(s):
                   if unicodedata.category(c) != 'Mn')
  
 def del_punct_wsp(text): 
+    text = text.upper()
+    text = re.sub('(?:MR|SR|SRES|MISS)\.\s*','',text)
+    text = re.sub(r'\([^)]*\)', '', text) #remueve paréntesis y todo lo de adentro
     text = text.replace(".","").replace('('," ").replace(")"," ")
     text = text.replace("\M:"," ").replace("M:"," ")
     text = re.sub(r'[!"\#\$%\'\(\)\*\+,\-\./:;<=>\?@\[\\\]\^_`\{\|\}\~]',' ',text) #borra punct y agrega espacio
-    text = re.sub(r'^\d+$',' ', text)
+    text = re.sub(r'\d+\b',' ', text)
     text = strip_accents(text)
+    return text
+
+# =============================================================================
+# glei = pd.read_csv('https://www.gleif.org/content/2-about-lei/7-code-lists/2-iso-20275-entity-legal-forms-code-list/2020-11-19_elf-code-list-v1.3.csv')
+# ab = ";".join(glei[glei['Abbreviations Local language'].isna()==False]["Abbreviations Local language"].drop_duplicates().values.tolist())
+# ab = np.unique(np.array(ab.split(";"))).tolist()
+# abreviaturas = np.unique(np.array([x.upper() for x in ab])).tolist()+["REF"]
+# indices = list(range(1,len(abreviaturas)+1))
+# abrev_dict = dict()
+# for k,v in zip(abreviaturas, indices): 
+#   abrev_dict[k]=v
+# =============================================================================
+
+def special_corrections(text): 
+    text = re.sub(r"\bCOMPA.*IA\b","COMPANIA",text)
+    text = re.sub(r"\bS A I C Y A\b","SAICYA", text)
+    text = re.sub(r"\bS A C I\b","SACI",text)
+    text = re.sub(r"\bSAIC Y F\b","SAICYF", text)
+    text = re.sub(r"\bSA IC Y F\b","SAICYF",text)
+    text = re.sub(r"\bPROD Y SERVICIOS\b","PRODUCTOS Y SERVICIOS",text)
+    text = re.sub(r"\bSA\b|\bS A\b|\bSOCIEDAD ANONIMA\b","SA", text)
+    text = re.sub(r"\bS R L\b|\bSOCIEDAD DE RESPONSABILIDAD LIMITADA\b","SRL", text)
     return text
 
 def acronyms(text): 
@@ -62,7 +89,8 @@ def pre_processing(text,
                    digits=True, 
                    strip_space=True, 
                    acronyms_at_end=True,
-                   special_deletions = None): 
+                   special_deletions = None,
+                   specialcorr=True): 
     
     """1) Se borra puntuación, acentos y caracteres específicos como "\M:"
        2) Se borran dígitos
@@ -89,9 +117,50 @@ def pre_processing(text,
       #print(text)
     if within_spaces:
       text = remove_within_wsp(text)  
+    if specialcorr: 
+        text=special_corrections(text)
 
     return text
 
 def ngrams(text, n=3): 
     ngrams = zip(*[text[i:] for i in range(n)])
     return [''.join(ngram) for ngram in ngrams]
+
+def AxB(listA, listaB, output_folder, vectorizing_by="A", analyze_by='word', lowerbound=0.8, topn=10, idfsmooth=True, sublinear=True): 
+    #Vectorizer
+    vectorizer = TfidfVectorizer(min_df=3, analyzer=analyze_by, lowercase=False, smooth_idf=idfsmooth, sublinear_tf=sublinear)
+    
+    ''' 
+    * vectorizing_by="A" es producto de AxB.transpose() 
+    y los features son de A
+    
+    * vectorizing_by="B" es producto de AxB.transpose()
+    y los features son de B
+    '''
+     
+    if vectorizing_by=="A": 
+        print("TF-IDF Vectorizig...\n")
+        A = vectorizer.fit_transform(listA)
+        B = vectorizer.transform(listaB)
+        print("Processing Matches...\n")
+    
+    if vectorizing_by=="B": 
+        print("TF-IDF Vectorizig...\n")
+        B = vectorizer.fit_transform(listaB)
+        A = vectorizer.transform(listA)
+        print("Processing Matches...\n")
+    #Sparse Matrix dot product
+    import time
+    t1 = time.time()
+    matches_ngrams = awesome_cossim_topn(A,B.transpose(), topn, lowerbound)
+    t = time.time()-t1
+    print('This program has runned in {} seconds\n'.format(t))
+    
+    #Saving Matrix
+    from scipy import sparse
+    from datetime import datetime
+    outputpath = output_folder+"/"+"matches_{}.npz".format(datetime.now().strftime('%Y-%m-%d %H_%M_%S'))
+    sparse.save_npz(outputpath, matches_ngrams)
+    print("Matches save into {}".format(outputpath))
+        
+    return matches_ngrams
--- matches.py
+++ matches.py
@@ -13,6 +13,9 @@ import time
 import matplotlib.pyplot as plt
 from functools import reduce
 from collections import Counter
+
+#Ruta directorio
+os.chdir("C:/Users/Asus/Desktop/CEP/text_mining_squared")
  
 #Paquetes NLP 
 import nltk
@@ -22,16 +25,20 @@ import unicodedata
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import csr_matrix
 from sparse_dot_topn import awesome_cossim_topn
-from CleaningRE import *
+
 
 
 #Lectura de datos
-names_minera = pd.read_excel("data/Cuits proveedores mineros.xlsx", sheet_name='Listado CUITs mineria')
+names_mineras = pd.read_excel("data/Cuits proveedores mineros.xlsx", sheet_name='Listado CUITs mineria')
 names_mectra = pd.read_excel("data/Cuits proveedores mineros.xlsx", sheet_name='Total CUITs')
-names_mectra = mectra[mectra.cuit!=' CHAUVIN GERIATRICA S.A.'].reset_index(drop=True)
+names_mectra = names_mectra[names_mectra.cuit!=' CHAUVIN GERIATRICA S.A.'].reset_index(drop=True)
 
+#Obtengo Nombres
+names_mineras = names_mineras[names_mineras.cuit.isna()].reset_index(drop=True)
+names_mectra = names_mectra[names_mectra.denominacion.isna()==False].reset_index(drop=True)
 
 #Regular Expressions
+from CleaningRE import *
 names_mineras['clean1'] = names_mineras['razon social'].apply(lambda x: pre_processing(str(x),
                                                                                   digits=False,
                                                                                   acronyms_at_end=False,
@@ -45,68 +52,66 @@ names_mectra['clean1'] = names_mectra['denominacion'].apply(lambda x: pre_proces
 
 
 
+merged = names_mineras.merge(names_mectra, how='left', left_on='clean1', right_on='clean1')
 
-#merged = names_mineras.merge(names_mectra, how='left', left_on='clean1', right_on='clean1')
+ok = merged[merged.denominacion.isna()==False]['clean1'].to_numpy()
 
-def ngrams2(text, n=2): 
+
+def ngrams2(text, n=3): 
     ngrams = zip(*[text[i:] for i in range(n)])
     return [''.join(ngram) for ngram in ngrams]
 
-#Representacion Matricial
-
-def AxB(listA, listaB, output_folder, vectorizing_by="A", analyze_by='word', lowerbound=0.8, topn=10, idfsmooth=True, sublinear=True): 
-    #Vectorizer
-    vectorizer = TfidfVectorizer(min_df=3, analyzer=analyze_by, lowercase=False, smooth_idf=idfsmooth, sublinear_tf=sublinear)
-    
-    ''' 
-    * vectorizing_by="A" es producto de AxB.transpose() 
-    y los features son de A
-    
-    * vectorizing_by="B" es producto de AxB.transpose()
-    y los features son de B
-    '''
-     
-    if vectorizing_by=="A": 
-        print("TF-IDF Vectorizig...\n")
-        A = vectorizer.fit_transform(listA)
-        B = vectorizer.transform(listaB)
-        print("Processing Matches...\n")
-    
-    if vectorizing_by=="B": 
-        print("TF-IDF Vectorizig...\n")
-        B = vectorizer.fit_transform(listaB)
-        A = vectorizer.transform(listA)
-        print("Processing Matches...\n")
-    #Sparse Matrix dot product
-    import time
-    t1 = time.time()
-    matches_ngrams = awesome_cossim_topn(A,B.transpose(), topn, lowerbound)
-    t = time.time()-t1
-    print('This program has runned in {} seconds\n'.format(t))
-    
-    #Saving Matrix
-    from scipy import sparse
-    from datetime import datetime
-    outputpath = output_folder+"/"+"matches_{}.npz".format(datetime.now().strftime('%Y-%m-%d %H_%M_%S'))
-    sparse.save_npz(outputpath, matches_ngrams)
-    print("Matches save into {}".format(outputpath))
-        
-    return matches_ngrams
-    
 
 NameMineras = names_mineras.clean1.unique().tolist()
 NameMectra = names_mectra.clean1.unique().tolist()
-    
-matches = AxB(NameMineras, NameMectra, output_folder='data',
-              analyze_by=ngrams2,
-              vectorizing_by="B", lowerbound=0.90,
-              topn=10, idfsmooth=False, sublinear=False)
 
-print(matches[:,:])
+all_names = NameMineras + NameMectra
+
+vectorize_all = TfidfVectorizer(min_df=3, analyzer=ngrams2, lowercase=False, smooth_idf=False, sublinear_tf=False)
+tfidf_matrix_all = vectorize_all.fit(all_names)
+
+tfidf_mineras = vectorize_all.transform(NameMineras)
+tfidf_mectra = vectorize_all.transform(NameMectra)
+
+match = awesome_cossim_topn(tfidf_mineras, tfidf_mectra.transpose(), ntop=1, lower_bound=0.80)
+
+ver = np.nonzero(match.toarray())
+
+Names = pd.DataFrame()
+Names['NameMineras'] = NameMineras
+Names['NameMectra'] = 'no_match'
+Names['Similarity'] = np.nan
+
+for k,v in zip(ver[0],ver[1]):
+    Names.iloc[k,1]=NameMectra[v]
+    Names.iloc[k,2] = match[k,v]
+
+ok_plus = Names[Names.NameMectra!="no_match"]['NameMineras'].to_numpy()
 
 
-print(NameMectra[490500])
-print(NameMineras[544])
+Names.to_excel('data/primer_output.xlsx',index=False)
+
+np.setdiff1d(ok_plus,ok)
+names_mectra[names_mectra.clean1.str.contains("GOLDER")]
 
 
+pre_processing("2219292 ALBER23TA LTD", acronyms_at_end=False, digits=False)
 
+a = names_mineras[names_mineras['clean1']=="PRINGLES  SR LEONARDO ALBERTO"]['razon social'].values[0]
+
+names_mectra[names_mectra.clean1.str.contains(r'(?=.*VIDELA)(?=.*JULIO)', regex=True)]['clean1']
+
+b = names_mectra[names_mectra.clean1.str.contains('PATRICIO PALMERO', regex=True)]['clean1']
+
+c = names_mectra[names_mectra.clean1==b]['denominacion'].values[0]
+
+names_mectra[names_mectra.clean1.str.contains('VALLE DEL CURA', regex=True)] 
+
+pre_processing(a, digits=False,acronyms_at_end=False,special_deletions=False)
+
+re.sub('(?:MR|SR|SRES|MISS)\.\s*','',a.upper())
+
+re.sub(r"COMPAďż˝IA|COMPADZ˝IA|COMPA#IA","COMPANIA",c) 
+
+prefixes = ['mr', 'smr']
+print('\b(?:' + '|'.join(prefixes) + r')\.\s*')
